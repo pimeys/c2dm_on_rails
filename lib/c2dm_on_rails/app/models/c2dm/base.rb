6084dd9095d96bd7c9af9fb97c19341bbe5918eb
Orvar Segerstrom <OrvarSegerstrom@gmail.com>
2012-05-14 21:02:44 +0200

    Added table name explicitly in device and notification models

--- lib/c2dm_on_rails/app/models/c2dm/base.rb
+++ lib/c2dm_on_rails/app/models/c2dm/base.rb
@@ -1,11 +1,5 @@
 module C2dm
   class Base < ActiveRecord::Base # :nodoc:
-    
     self.abstract_class = true
-    
-    def self.table_name # :nodoc:
-      self.to_s.gsub("::", "_").tableize
-    end
-    
   end
 end
