857914cd4f57c6b55032e578182cbf7679ee1ff6
Orvar Segerstrom <OrvarSegerstrom@gmail.com>
2012-05-14 20:59:54 +0200

    Made C2dm::Base abstract

--- lib/c2dm_on_rails/app/models/c2dm/base.rb
+++ lib/c2dm_on_rails/app/models/c2dm/base.rb
@@ -1,6 +1,8 @@
 module C2dm
   class Base < ActiveRecord::Base # :nodoc:
     
+    self.abstract_class = true
+    
     def self.table_name # :nodoc:
       self.to_s.gsub("::", "_").tableize
     end
